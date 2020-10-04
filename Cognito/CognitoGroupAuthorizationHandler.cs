3dceba0880504eb6e813abcb39ceef2f4a937855
lucdv <lucdv@edmicro.com>
2020-10-04 22:29:34 +0700

    update

---
+++ Cognito/CognitoGroupAuthorizationHandler.cs
@@ -0,0 +1,24 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ServerlessSpaWithDotNet
+{
+    class CognitoGroupAuthorizationHandler : AuthorizationHandler<CognitoGroupAuthorizationRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, CognitoGroupAuthorizationRequirement requirement)
+        {
+            if (context.User.HasClaim(c => c.Type == "cognito:groups" &&
+                                           c.Value == requirement.CognitoGroup))
+            {
+                context.Succeed(requirement);
+            }
+            else
+            {
+                context.Fail();
+            }
+
+            return Task.CompletedTask;
+        }
+
+    }
+}
